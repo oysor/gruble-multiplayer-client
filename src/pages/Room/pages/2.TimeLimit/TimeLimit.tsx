777990028148fa1065be30e0a2e4e718e616808c
oysor <osorby@gmx.com>
2021-03-25 22:49:55 +0100

    Add text above time limit input

--- src/pages/Room/pages/2.TimeLimit/TimeLimit.tsx
+++ src/pages/Room/pages/2.TimeLimit/TimeLimit.tsx
@@ -29,6 +29,7 @@ export const TimeLimit: FunctionComponent = () => {
 
   return (
     <div className="room-time">
+      <span style={{ color: 'grey' }}>Set time limit</span>
       <form>
         <SmartNumericInput onChange={setTime} onKeyPress={handleKeyPress} value={time} />
         <SubmitButton
