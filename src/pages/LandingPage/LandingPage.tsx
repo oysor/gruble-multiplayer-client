32c82d252d696c828a202c82e6fce2c55caa1aae
oysor <o@regap.no>
2024-02-13 20:47:07 +0100

    Add styled components

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,11 +1,18 @@
 import React, { FunctionComponent } from 'react'
 import { Link } from 'react-router-dom'
 
-export const LandingPage: FunctionComponent = () => {
+import Styled from 'styled-components'
+
+const Title = Styled.h1`
+  color: red;
+`
+interface Props {}
+
+export const LandingPage: FunctionComponent = ({}: Props) => {
   return (
     <div className="landing-page">
       <Link to="/room" className="router-link">
-        Game room
+        <Title> Game room </Title>
       </Link>
       <Link to="/player" className="router-link">
         Player room
