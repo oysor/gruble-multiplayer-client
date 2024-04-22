6d1f00c7a04e5f3d5b2740158c0cb545c06ce9c8
oysor <o@regap.no>
2024-04-22 11:46:18 +0200

    Update input fields

--- src/common/components/inputs/SmartNumericInput.tsx
+++ src/common/components/inputs/SmartNumericInput.tsx
@@ -4,8 +4,10 @@ import { styled } from 'styled-components'
 interface SmartNumericInputProps {
   onChange: (value: number) => void
   onKeyPress?: (e: React.KeyboardEvent) => void
-  value?: number
+  onBlur?: (number: number) => void
+  value?: number | string
   className?: string
+  placeholder?: string
 }
 
 export const StyledInput = styled.input`
@@ -39,6 +41,68 @@ export const SmartNumericInput: FunctionComponent<SmartNumericInputProps> = ({
         onChange(parseInt(ev.target.value))
       }
       onKeyPress={(e) => onKeyPress(e)}
+    />
+  )
+}
+
+export const TransparentNumericInput = styled.input`
+  display: inline-block;
+  font-size: 1em;
+  text-align: center;
+  text-decoration: none;
+  padding-bottom: 0.2em;
+  margin: 0;
+  border: 0;
+  outline: none;
+  -webkit-appearance: none;
+  margin: 0;
+  -moz-appearance: textfield;
+
+  border: none;
+
+  color: var(--btnColor);
+  background-color: transparent;
+  border-bottom: 0.1em solid white;
+  font-family: var(--font-regular);
+
+  &:focus {
+    border-color: var(--pondr-pink);
+    caret-color: var(--pondr-pink);
+  }
+`
+
+export const PondrNumericInput: FunctionComponent<SmartNumericInputProps> = ({
+  onChange,
+  onBlur,
+  placeholder = '0',
+  value,
+  className = '',
+}) => {
+  return (
+    <TransparentNumericInput
+      className={className}
+      type="number"
+      value={value}
+      placeholder={placeholder}
+      step="1"
+      min="0"
+      max="99"
+      onChange={(ev: React.ChangeEvent<HTMLInputElement>): void => {
+        const numericInput = parseInt(ev.target.value)
+        const isNumber = !isNaN(numericInput)
+        if (isNumber) {
+          onChange(numericInput)
+        }
+      }}
+      onBlur={(ev: React.ChangeEvent<HTMLInputElement>) => {
+        const numericInput = parseInt(ev.target.value)
+        const isNumber = !isNaN(numericInput)
+        if (isNumber) {
+          if (onBlur) {
+            onBlur(numericInput)
+          }
+        }
+      }}
     />
   )
 }
